01503d5baf84ac2479af16d8ee21f61a22baac6c
Alcazar Arturo <alcazararturo@hotmaail.com>
2020-09-19 21:22:09 -0500

    Cambios sobre bandas

--- index.js
+++ index.js
@@ -2,26 +2,25 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
-//App de Express
+// App de Express
 const app = express();
+
 // Node Server
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
-// Mensajes de Sockets
-io.on('connection', client => {
-    console.log('Cliente conectado');
-    client.on('disconnect', () => {
-        console.log('Cliente desconectado');
-    });
-});
 
-//Path público
+// Path público
 const publicPath = path.resolve( __dirname, 'public' );
-app.use( express.static( publicPath));
+app.use( express.static( publicPath ) );
 
-server.listen(process.env.PORT, (err) => {
-    if (err) throw new Error(err);
-    console.log(`Servidor corriendo en el puerto ${ process.env.PORT }`);
+
+server.listen( process.env.PORT, ( err ) => {
+
+    if ( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT );
+
 });
+
+
